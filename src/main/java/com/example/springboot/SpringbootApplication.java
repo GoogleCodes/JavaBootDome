0689dbdc884c667e554848c420d0e2f4d019e7d2
‘511810970@qq.com <511810970@qq.com>
2018-02-14 09:32:21 +0800

    update

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -14,6 +14,7 @@ import javax.servlet.annotation.MultipartConfig;
 @SpringBootApplication
 public class SpringbootApplication extends SpringBootServletInitializer {
 
+	//
 
 	@Value("${tmpLocation}")
 	private String tmpLocation;
